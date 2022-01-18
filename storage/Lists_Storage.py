ee1437927ebedd765b19fb8e4a90fdb8517824ac
H-Bombmxpwr <97134010+H-Bombmxpwr@users.noreply.github.com>
2022-01-18 06:07:34 +0000

    Added the Connor function

--- storage/Lists_Storage.py
+++ storage/Lists_Storage.py
@@ -2,3 +2,5 @@ days = ["Thursday","thursday","thurday","4th day of the week"]
 
 
 thedan = ["steely dan","Steely dan","the dan","donald and walter","don and walt","walt and don","walter and donald"]
+
+conor_words = ["foreskin", "anatomically jewish","leviticus","vessel"]
