d30c583a46ad5e37706ceb3112eb1461f47d3c17
H-Bombmxpwr <97134010+H-Bombmxpwr@users.noreply.github.com>
2022-02-02 07:55:24 +0000

    activated trie

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def buildTrie():
 @client.event
 async def on_ready():
   print("Trie is building......")
-  #buildTrie()
+  buildTrie()
   print("Trie is built. Ready to read messages.\n")
   print('{0.user} is back online'.format(client))
   print('=------------------------------=')
