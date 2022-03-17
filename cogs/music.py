316a1119d8929844255e54402df3026a6b9ad144
H-Bombmxpwr <97134010+H-Bombmxpwr@users.noreply.github.com>
2022-03-17 03:33:51 +0000

    Start of adding a queue to the bot

--- cogs/music.py
+++ cogs/music.py
@@ -13,6 +13,7 @@ class Music(commands.Cog):
   """
   def __init__(self, client):
         self.client = client
+        self.qu = {}
 
   @commands.command(help = 'Info for the music commands')
   async def music(self,ctx):
@@ -32,12 +33,61 @@ class Music(commands.Cog):
         else:
             await ctx.voice_client.move_to(voice_channel)
   
-   
+  @commands.command(help = "Clear the queue",aliases = ['cq'])
+  async def clearqueue(self,ctx):
+    self.qu = {}
+    await ctx.send("The queue has been cleared")
+  
   @commands.command(help = 'Leave a voice channel', aliases = ['dis','leave'])
   async def disconnect(self,ctx):
         await ctx.voice_client.disconnect()
+        self.qu = {}
+        ctx.voice_client.cleanup()
         await ctx.send("Cya!")
+        
 
+
+  @commands.command(help = "Play skip the queue",aliases = ['ps'])
+  async def playskip(self,ctx,*,song:str = None):
+    if song == None:
+        await ctx.send("Please send the title of a song to play")
+        return
+    await ctx.invoke(self.client.get_command('join'))
+    self.client.song = str(song)
+    self.qu = {}
+    #search the song/ check if a url was sent
+    if str(song).find("https://www.youtube.com") == -1:
+          yt = YoutubeSearch(str(song), max_results=1).to_json()
+          song_id = str(json.loads(yt)['videos'][0]['id'])
+          url = "https://www.youtube.com/watch?v=" + song_id
+          if url == None:
+            await ctx.send("Song not found")
+            return
+    else:
+          url = str(song)
+
+    with suppress(AttributeError):
+      await ctx.trigger_typing()  
+
+    #plays the url generated above
+    FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+    YDL_OPTIONS = {'format':"bestaudio"}
+    vc = ctx.voice_client
+    try:
+      with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
+        info = ydl.extract_info(url, download=False)
+        url2 = info['formats'][0]['url']
+        source = await discord.FFmpegOpusAudio.from_probe(url2, **FFMPEG_OPTIONS)
+            
+    except:
+      await ctx.send("There was an error getting that song\nIt could be age restricted or private. Try a different song")
+      return
+    vc.stop()
+    vc.play(source)
+    self.qu[0] = source
+    await ctx.send("Now playing: " + str(info.get('title', None)) + '\n' + url)
+
+    
   @commands.command(help = 'Play a song')
   async def play(self,ctx,*,song:str = None):
 
@@ -47,6 +97,7 @@ class Music(commands.Cog):
         #get the bot to join the player if it isnt already in it
         await ctx.invoke(self.client.get_command('join'))
         self.client.song = str(song)
+          
         #search the song/ check if a url was sent
         if str(song).find("https://www.youtube.com") == -1:
           yt = YoutubeSearch(str(song), max_results=1).to_json()
@@ -60,9 +111,8 @@ class Music(commands.Cog):
 
         with suppress(AttributeError):
           await ctx.trigger_typing()  
-        
+
         #plays the url generated above
-        ctx.voice_client.stop()
         FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
         YDL_OPTIONS = {'format':"bestaudio"}
         vc = ctx.voice_client
@@ -71,24 +121,38 @@ class Music(commands.Cog):
             info = ydl.extract_info(url, download=False)
             url2 = info['formats'][0]['url']
             source = await discord.FFmpegOpusAudio.from_probe(url2, **FFMPEG_OPTIONS)
-            vc.play(source)
-            await ctx.send("Now playing: " + info.get('title', None) + '\n' + url)
             
         except:
           await ctx.send("There was an error getting that song\nIt could be age restricted or private. Try a different song")
-    
+          return
+
+        
+        #checking if add to the queue or play the song instantly
+
+        print(self.qu)
+        if len(self.qu) == 0:
+          self.start_playing(vc, source)
+          await ctx.send("Now playing: " + str(info.get('title', None)) + '\n' + url)
+        else:
+          self.qu[len(self.qu)] = source
+          await ctx.send(str(info.get('title',None)) + " was added to the queue")
+        
+
+
+  #pause the player
   @commands.command(help = 'Pause the current song',aliases = ['pa'])
   async def pause(self,ctx):
         await ctx.send("Paused ⏸")
         await ctx.voice_client.pause()
         
-    
+  #resume the player
   @commands.command(help = 'Resume the current song', aliases = ['res'])
   async def resume(self,ctx):
         await ctx.send("Resumed ▶️")
         await ctx.voice_client.resume()
 
 
+    #get lyrics for the current song or any song
   @commands.command(help="Get lyrics for a song, send without a song arguement when playing a song in the voice channel to get the current songs lyrics", aliases=["ly"])
   async def lyrics(self, ctx, *, song = None):
         if song == None and ctx.voice_client.is_connected():
@@ -118,6 +182,21 @@ class Music(commands.Cog):
         embedVar.set_footer(text=  'Requested by ' + str(ctx.author.name),icon_url = ctx.author.avatar)
         await ctx.send(embed = embedVar)
 
+
+  def start_playing(self,vc,source):
+      print("got into player")
+      self.qu[0] = source
+    
+      i = 0
+      while i <  len(self.qu):
+        try:
+          vc.play(self.qu[i], after=lambda e: print('Player error: %s' % e) if e else None)
+
+        except:
+          pass
+          i += 1
+      print("Exited the loop with queue of:")
+      print(self.qu)
 
 
 async def setup(client):
